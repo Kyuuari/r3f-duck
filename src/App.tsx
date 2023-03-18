fb24e304517a0613ac636b56c9510bfe756d77c8
Chester Cari <chester_cari@outlook.com>
2023-03-18 13:54:40 -0400

    added models

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
 import "./App.css";
+import Ducks from "./components/Duck";
 
 function App() {
+  const [speed, set] = useState<number>(1); // specify number type for speed state and set function
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <Ducks speed={speed} />
+      <input
+        className="absolute bottom-1/2 -rotate-90"
+        type="range"
+        min="0"
+        max="10"
+        value={speed}
+        step="1"
+        onChange={(e) => set(Number(e.target.value))} // convert string value to number
+      />
+    </>
   );
 }
 
